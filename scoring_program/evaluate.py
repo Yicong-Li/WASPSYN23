3e29641b5a80b6b137958dcd07f693102dc4e2ab
Yicong-Li <jackson_li_0225@163.com>
2023-01-17 19:07:01 -0500

    update evaluate.py  and .gitignore

--- scoring_program/evaluate.py
+++ scoring_program/evaluate.py
@@ -2,32 +2,53 @@ import h5py
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
-# read data
-path = 'datasets/training_set/train_sample3_vol1/syns_zyx_3680-4096_2944-3360_4448-4864.h5'
-f = h5py.File(path, 'r')
-pre_gt = f['pre'][:]
-f.close
-
-# submission
-pre_test = np.random.randint(1, 1000, (160, 3))
-
 # calculate cost: l2-norm
-pre_cost_matrix = np.zeros((pre_test.shape[0], pre_gt.shape[0]))
-for i in range(pre_gt.shape[0]):
-    diff = pre_test - pre_gt[i, :]
-    square = np.power(diff, 2)
-    sum_of_square = np.sum(square, axis=1)
-    cost = np.sqrt(sum_of_square)
-    pre_cost_matrix[:, i] = cost
+def cost_matrix(test, gt):
+    matrix = np.zeros((test.shape[0], gt.shape[0]))
+    for i in range(gt.shape[0]):
+        diff = test - gt[i, :]
+        square = np.power(diff, 2)
+        sum_of_square = np.sum(square, axis=1)
+        cost = np.sqrt(sum_of_square)
+        matrix[:, i] = cost
+    return matrix
 
-# assignments
-row_ind, col_ind = linear_sum_assignment(pre_cost_matrix)
-pre_test_id = np.arange(pre_test.shape[0])[row_ind]
-pre_gt_id = np.arange(pre_gt.shape[0])[col_ind]
-assignments = dict(zip(pre_test_id, pre_gt_id))
-associated_cost = pre_cost_matrix[row_ind, col_ind].sum()
-fp_and_fn = np.abs(pre_test.shape[0] - pre_gt.shape[0])
-print('assignments:', assignments)
-print('cost:', associated_cost)
-print('count of false negative & false positive:', fp_and_fn)
-print(row_ind)
+# assign and calculate f-score
+def assign_cal_f1(test, gt, radius, use_radius):
+    cost_m = cost_matrix(test, gt)
+
+    row_ind, col_ind = linear_sum_assignment(cost_m)
+    final_row_ind = list(row_ind)
+    final_col_ind = list(col_ind)
+    if use_radius:
+        r = radius
+        for i in list(zip(row_ind, col_ind)):
+            if cost_m[i[0], i[1]] > r:
+                final_row_ind.remove(i[0])
+                final_col_ind.remove(i[1])
+    test_id = np.arange(test.shape[0])[final_row_ind]
+    gt_id = np.arange(gt.shape[0])[final_col_ind]
+    assignments = dict(zip(test_id, gt_id))
+    associated_cost = cost_m[final_row_ind, final_col_ind].sum()
+
+    fp_fn_1 = np.abs(test.shape[0] - gt.shape[0])
+    fp_fn_2 = (len(list(zip(row_ind, col_ind))) - len(list(zip(final_row_ind, final_col_ind)))) * 2
+    tp = len(list(zip(final_row_ind, final_col_ind)))
+    fscore = 2 * tp / (2 * tp + fp_fn_1 + fp_fn_2)
+    return assignments, associated_cost, fscore
+
+# read data
+path_gt = 'datasets/training_set/train_sample3_vol1/syns_zyx_3680-4096_2944-3360_4448-4864.h5'
+path_test = 'datasets/training_set/train_sample3_vol0/syns_zyx_2217-2617_4038-4448_6335-6735.h5'
+f_gt = h5py.File(path_gt, 'r')
+f_test = h5py.File(path_test, 'r')
+pre_gt = f_gt['pre'][:]
+pre_test = f_test['pre'][:]
+f_gt.close
+f_test.close
+
+# evaluation
+pre_assignments, pre_associated_cost, pre_fscore = assign_cal_f1(pre_test, pre_gt, 2600, True)
+print('pre_assignments:', pre_assignments)
+print('pre_cost:', pre_associated_cost)
+print('pre_fscore:', pre_fscore)
